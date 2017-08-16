a4ed8a5569233163c9521211b16c5bb4b32fcd0a
rsawyer <rsawyer@tripwire.com>
2017-08-16 16:41:20 -0700

    Removed app from blog because I really want to figure out how to inject it into the index.

--- templates/views/pages/Blog/Blog.jsx
+++ templates/views/pages/Blog/Blog.jsx
@@ -1,27 +1,25 @@
 import React, {Component} from 'react';
 
-import App from '../App';
 import BlogContent from '../../components/BlogContent';
 import BlogCategories from '../../components/BlogCategories';
 
 class Blog extends Component {
+
   render(props) {
     let {data} = this.props;
     return (
-      <App {...this.props}>
-        <div className="container padding-bottom">
-        {
-          data.categories.length > 0 && (
-            <div className="row">
-              <BlogCategories data={data} />
-              <BlogContent data={data} />
-            </div>
-          )
-        }
-        </div>
-      </App>
+      <div className="container padding-bottom">
+      {
+        data.categories.length > 0 && (
+          <div className="row">
+            <BlogCategories data={data} />
+            <BlogContent data={data} />
+          </div>
+        )
+      }
+      </div>
     )
   }
 }
 
-module.exports = Blog;
+export default Blog;
